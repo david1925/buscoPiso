38c80981863e882c9bdc05e4ef54a39e08f62ba6
Blancafort5 <polblancafortrivas@gmail.com>
2017-11-16 00:10:31 +0100

    Index Header

---
+++ js/controller/index.js
@@ -0,0 +1,6 @@
+(function(){
+    angular.module("buscoPiso").controller("IndexController", ['$scope', '$http', '$translate', 'Domain', '$window', function($scope, $http, $translate, Domain, $window) {
+    
+
+    }]);
+  })();
