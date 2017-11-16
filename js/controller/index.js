4f1f5036e236b73817e8fcddaba2e05c8a26db7c
Blancafort5 <polblancafortrivas@gmail.com>
2017-11-16 14:11:41 +0100

    Contact

--- js/controller/index.js
+++ js/controller/index.js
@@ -2,5 +2,6 @@
     angular.module("buscoPiso").controller("IndexController", ['$scope', '$http', '$translate', 'Domain', '$window', function($scope, $http, $translate, Domain, $window) {
     
 
+      $scope.login    = Domain + "views/login.html";
     }]);
   })();
