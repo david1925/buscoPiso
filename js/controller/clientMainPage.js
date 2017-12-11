aabad5bee16418b170ee2f1535f36d3118ba78a5
Blancafort5 <polblancafortrivas@gmail.com>
2017-12-11 13:50:18 +0100

    Sessiones
    Al hacer Logout y volver a 'index.html' da error de JSON

--- js/controller/clientMainPage.js
+++ js/controller/clientMainPage.js
@@ -1,5 +1,30 @@
 (function(){
     angular.module("buscoPiso").controller("ClientMainPageController", ['$scope', '$http', '$translate', 'Domain', '$window', function($scope, $http, $translate, Domain, $window) {
 
+        $http.get(Domain + "api/public/users/login/check")
+            .then(function(response) {
+                $scope.isLoged = response.data;
+                if ($scope.isLoged == "true") {
+                    $scope.user = new Users();
+                    var userObj = JSON.parse(sessionStorage.userLogged);
+                    $scope.user.construct(
+                        userObj.users_id_user,
+                        userObj.users_name,
+                        userObj.users_firstname,
+                        userObj.users_lastname,
+                        userObj.users_email,
+                        userObj.users_password,
+                        userObj.users_phone,
+                        userObj.floor_description,
+                        userObj.state,
+                        userObj.last_login,
+                        userObj.groups_groups_id_group,
+                        userObj.business_office_idoffice,
+                        userObj.additional_features_users_idadditional_features_users
+                    );
+                }
+            }
+        );
+        
     }]);
 })();
